6368bea37a65defd7397c3a36c6724b520a607a1
warriorZH <zhangpeihua123@gmail.com>
2016-05-30 01:07:16 +0800

    predict system base on arma vertion-1

--- tianchi/armaTest.py
+++ tianchi/armaTest.py
@@ -18,35 +18,33 @@ from statsmodels.tsa.arima_model import _arma_predict_out_of_sample
 
 from statsmodels.graphics.api import qqplot
 
-
-
-
 # print(sm.datasets.sunspots.NOTE)
-s_dta = [216,211,213,187,208,231,239,291,173,253,317,235,196,271,221,252,181,222,218,247,272,233,215,264,205,282,213,271,258,228,222,256,361,282,253,300,239,261,281,257,286,276,387,281,300,323,332,343,315,318,263,329,291,352,335,357,390,328,246,232,281,354,353,332,300,282,222,350,252,316,303,234,264,377,307,412,336,368,353,407,407,350,419,480,378,427,410,339,295,312,338,415,323,285,320,373,358,328,441,441,359,306,338,312,353,312,319,353,396,341,359,427,389,522,372,467,407,511,514,427,385,498,527,495,418,392,537,465,422,422,439,503,363,352,469,411,422,487,389,299,360,503,439,490,315,487,603,606,366,556,463,405,408,429,358,477,418,370,334,376,317,304,348,326,305,353,425,355,372,374,351,356,359,301,276,321,407,379,406,439,411,417,319]
-dta = pd.Series(s_dta[:153])
-dta.index = pd.Index(sm.tsa.datetools.dates_from_range('2001', '2153'))
-dta = dta.diff(1)
-# # del dta["YEAR"]
+s_dta = [304,275,291,3993,5769,2740,1676,1448,1563,1578,3111,3232,2063,1436,1370,1515,1595,2808,2507,2400,2144,2136,2305,1724,3464,5462,4223,2895,3124,3408,3302,2234,1949,2239,2490,2221,2105,1970,2463,1937,1831,1370,1618,1546,1441,1544,1309,1233,1182,1412,1069,874,848,950,740,936,861,1038,1027,845,976,1112,1155,831,1053,828,869,976,882,964,1013,604,810,702,792,692,812,867,799,866,717,851,789,727,893,1127,847,787,799,802,717,868,1068,950,702,792,787,672,685,697,838,634,829,778,743,728,755,1305,936,800,937,735,590,816,828,652,631,710,689,649,836,747,568,551,772,688,758,607,749,837,669,654,784,728,600,753,786,708,570,616,742,717,844,633,511,395,744,458,436,656,787,572,500,632,724,702,641,607,641,531,503,566,868,710,725,908,852,732,618,1169,887,700,502,798,455,634,459,510,588,607,484,423,386]
+dta = pd.Series(s_dta[60:123])
+dta.index = pd.Index(sm.tsa.datetools.dates_from_range('2061', '2123'))
+# dta = dta.diff(1)
 plt.plot(dta.index,dta.values)
-# print(dta.values)
 plt.show()
-#
-fig = plt.figure(figsize=(12,8))
-ax1 = fig.add_subplot(211)
-fig = sm.graphics.tsa.plot_acf(dta.values.squeeze(), lags=40, ax=ax1)
-ax2 = fig.add_subplot(212)
-fig = sm.graphics.tsa.plot_pacf(dta, lags=40, ax=ax2)
-fig.show()
+# #
+# fig = plt.figure(figsize=(12,8))
+# ax1 = fig.add_subplot(211)
+# fig = sm.graphics.tsa.plot_acf(dta.values.squeeze(), lags=40, ax=ax1)
+# ax2 = fig.add_subplot(212)
+# fig = sm.graphics.tsa.plot_pacf(dta, lags=40, ax=ax2)
+# fig.show()x, unbiased=False, nlags=40, qstat=False, fft=False, alpha=None
+
+p = sm.tsa.acf(dta, nlags=20)
+print(p)
+p = sm.tsa.pacf(dta, nlags=20)
+print(p)
+
 
 # arma_mod20 = sm.tsa.ARMA(dta, (2,2)).fit()
 # # print(arma_mod20.params)
 
-arma_mod30 = sm.tsa.ARMA(dta, (3, 3)).fit(trend="c", method="css-mle")
-# print(arma_mod30.params)
-
-
-
-#
+arma_mod30 = sm.tsa.ARMA(dta, (0, 1)).fit(disp=-1, trend="c", solver='powell', method="css")
+print(arma_mod30.summary())
+# # print(arma_mod30.params)
 # # print(arma_mod20.aic, arma_mod20.bic, arma_mod20.hqic)
 # # print(arma_mod30.aic, arma_mod30.bic, arma_mod30.hqic)
 # #
@@ -59,7 +57,7 @@ arma_mod30 = sm.tsa.ARMA(dta, (3, 3)).fit(trend="c", method="css-mle")
 # # fig.show()
 #
 # resid = arma_mod30.resid
-# # print(stats.normaltest(resid))
+# # print(stats.normaltest(resid-1))
 # fig = plt.figure(figsize=(12,8))
 # ax = fig.add_subplot(111)
 # fig = qqplot(resid, line='q', ax=ax, fit=True)
@@ -93,11 +91,11 @@ p = arma_mod30.k_ar
 q = arma_mod30.k_ma
 k_exog = arma_mod30.k_exog
 k_trend = arma_mod30.k_trend
-steps = 30
+steps = 20
 
-pre_result = _arma_predict_out_of_sample(params, steps, residuals, p, q, k_trend, k_exog, endog=dta, exog=None, start=len(dta))
+pre_result = _arma_predict_out_of_sample(params, steps, residuals, p, q, k_trend, k_exog, endog=dta, exog=None, method='ols', start=len(dta))
 # print "#################################"
 print(pre_result)
-plt.plot(s_dta[153:183])
-plt.plot(pre_result)
+plt.plot(s_dta[123:143])
+plt.plot(pre_result,'red')
 plt.show()
